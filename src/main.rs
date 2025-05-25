e2852b4a27381e377553d93608ff2656bea45ab2
UseK <use.k.james@gmail.com>
2025-05-25 10:16:54 +0900

    Snapshot: just learning Props

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{cell::Cell, str::FromStr};
 
 use dioxus::{
     html::mo::{self, movablelimits},
@@ -40,21 +40,46 @@ fn Lattice(shape: Shape, cell_pixel: usize) -> Element {
     );
     rsx! {
         div { class: "lattice", style,
-            for (_ , _ , is_wall) in shape().coordinates() {
-                LatticeCell { is_wall }
+            for (x , y , is_wall) in shape().coordinates() {
+                LatticeCell {
+                    onclick: move |_| shape().put_on(x, y, !is_wall),
+                    is_wall,
+                }
             }
         }
+        FancyButton { onclick: move |event| println!("{event:?}"), other: true }
     }
 }
 
-#[component]
-fn LatticeCell(is_wall: bool) -> Element {
+fn LatticeCell(props: CellProps) -> Element {
     rsx! {
         div {
             class: "lattice-cell",
             border: "1px solid #000",
             padding: "10px",
-            background_color: if is_wall { "gray" } else { "white" },
+            background_color: if props.is_wall { "gray" } else { "white" },
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct CellProps {
+    onclick: EventHandler<MouseEvent>,
+    is_wall: bool,
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct FancyButtonProps {
+    onclick: EventHandler<MouseEvent>,
+    other: bool,
+}
+
+fn FancyButton(props: FancyButtonProps) -> Element {
+    rsx! {
+        button {
+            class: "fancy-button",
+            onclick: move |evt| props.onclick.call(evt),
+            "click me pls."
         }
     }
 }
